78413bef41ab9d53c5f960600d307bd347a8749e
Ирина <irinabolgova047@gmail.com>
2024-03-14 20:05:55 +0500

    Рабочие тесты

---
+++ TRPO-Lab3.Library/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRPO_Lab3.Library
+{
+    public class MathLib1
+    {
+        public static double CalculateSphereSectorSurfaceArea(double radius, double angleInDegrees)
+        {
+            
+                if (radius <= 0 || angleInDegrees <= 0)
+                {
+                    throw new Exception();
+                }
+
+                double angleInRadians = angleInDegrees * Math.PI / 180;
+                double S = 2 * Math.PI * radius * radius * (1 - Math.Cos(angleInRadians));
+                return S;
+            
+            
+
+        }
+
+
+
+    }
+}
--- TRPO-Lab3.Tests/UnitTest1.cs
+++ TRPO-Lab3.Tests/UnitTest1.cs
@@ -18,7 +18,14 @@ namespace TRPO_Lab3.Tests
             var area = MathLib.CalculateSphereSectorSurfaceArea(radius, angleInDegrees);
             Assert.AreEqual(expected, area, 0.01);
         }
-       
+       [Test]
+        public void Test21()
+        {
+            const double radius = -6;
+            const double angleInDegrees = 60;
+            const double expected = 113.09;
+            Assert.Throws<Exception>(() => MathLib1.CalculateSphereSectorSurfaceArea(radius, angleInDegrees));
+        }
 
     }
 }
