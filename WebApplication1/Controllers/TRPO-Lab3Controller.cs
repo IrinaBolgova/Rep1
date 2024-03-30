1fbe692d2110346bf6e3d0da6416ce8ccade0c50
Ирина <irinabolgova047@gmail.com>
2024-03-30 11:24:26 +0500

    Добавление контроллера

---
+++ WebApplication1/Controllers/TRPO-Lab3Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TRPO_Lab3.WebApplication.Controllers
+{
+    public class TRPO_Lab3Controller : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
+}
